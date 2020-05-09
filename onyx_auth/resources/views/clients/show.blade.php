73d2dd7ef5705aba4ca869d453cf6bfa897876ad
meche.grc <meche.grc@gmail.com>
2020-05-09 17:28:24 -0400

    show client

--- onyx_auth/resources/views/clients/show.blade.php
+++ onyx_auth/resources/views/clients/show.blade.php
@@ -1,1 +1,26 @@
-saludo desde detalle cliente
+@extends('layouts.app')
+
+@section('content')
+   
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header d-flex justify-content-between align-items-center">
+                    <span>Client Details</span>
+                </div>
+
+                <div class="card-body">
+                    <h5 class="card-title">{{$client->name}}</h5>
+                    <p class="card-text">Address: {{$client->address}}</p>
+                    <p class="card-text">Phone: {{$client->phone}}</p>
+                    <p class="card-text">Email: {{$client->email}}</p>
+                    <a href="{{route('clients.edit', $client)}}" class="btn btn-primary">Edit</a>
+                    <a href="{{ URL::previous() }}" class="btn btn-secondary">Back</a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
