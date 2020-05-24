606e48c802128f5d243f28e7d1eea102347ffb20
meche.grc <meche.grc@gmail.com>
2020-05-23 22:11:29 -0400

    new skin home

--- onyx_auth/resources/views/home.blade.php
+++ onyx_auth/resources/views/home.blade.php
@@ -3,41 +3,64 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header"><h3>Dashboard</h3></div>
+			<div class="col-md-12">
+				<h2>Dashboard</h2>
+				<div class="card-deck mt-4">
 
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
-                    
+					<!-- PRODUCTS -->
+					<div class="card">
+						<div class="image-dashboard-card cyan">
+							<i class="fas fa-headphones-alt"></i>
+						</div>
+						
+						<div class="card-body">
+							<h5 class="card-title">Productos</h5>
+							<p class="card-text">En esta sección podrás gestionar los productos de tu inventario.</p>
+							<a href="{{ url('/products') }}" class="btn btn-primary">Ir a productos</a>
+						</div>
+					</div>
 
-                    <div class="row">
-                        <div class="col-md-4 col-sm-12"><a href="{{ url('/products') }}">
-                                <div class="card dashboard-card">
-                                    <img src="{{ asset('img/products-card.png') }}" class="card-img-top" alt="Product manager">
-                                    <div class="card-body">
-                                        <p class="card-text">Click aquí para gestionar los productos</p>
-                                    </div>
-                                </div>
-                            </a></div>
-                        <div class="col-md-4 col-sm-12"><a href="{{ url('/clients') }}">
-                                <div class="card dashboard-card">
-                                    <img src="{{ asset('img/clients-card.png') }}" class="card-img-top" alt="Product manager">
-                                    <div class="card-body">
-                                        <p class="card-text">Click aquí para gestionar los clientes</p>
-                                    </div>
-                                </div>
-                            </a></div>
+					<!-- CLIENTS -->
+					<div class="card">
+						<div class="image-dashboard-card yellow">
+							<i class="fas fa-user-friends"></i>
+						</div>
+						
+						<div class="card-body">
+							<h5 class="card-title">Contactos</h5>
+							<p class="card-text">En esta sección podrás gestionar los datos de tus contactos.</p>
+							<a href="{{ url('/clients') }}" class="btn btn-primary">Ir a contactos</a>
+						</div>
+					</div>
 
-                    </div>
+					<!-- BUDGETS -->
+					<div class="card">
+						<div class="image-dashboard-card red">
+							<i class="fas fa-hand-holding-usd"></i>
+						</div>
+						
+						<div class="card-body">
+							<h5 class="card-title">Presupuestos</h5>
+							<p class="card-text">En esta sección podrás crear, editar y descargar tus presupuestos.</p>
+							<a href="{{ url('/budgets') }}" class="btn btn-primary">Ir a presupuestos</a>
+						</div>
+					</div>
 
-                </div>
-            </div>
-        </div>
+					<!-- Invoices -->
+					<!--<div class="card">
+						<div class="image-dashboard-card green">
+							<i class="fas fa-file-invoice-dollar"></i>
+						</div>
+						
+						<div class="card-body">
+							<h5 class="card-title">Facturas</h5>
+							<p class="card-text">En esta sección podrás crear, editar y descargar tus facturas.</p>
+							<a href="#" class="btn btn-primary">Ir a facturas</a>
+						</div>
+					</div>-->
+				
+				</div>
+			</div>
     </div>
 </div>
 @endsection
