0dd452c4d59b70afbe8d6e57b50608ead2fad96d
meche.grc <meche.grc@gmail.com>
2020-05-30 18:46:12 -0400

    excel provider

--- onyx_auth/app/Exports/BudgetsExport.php
+++ onyx_auth/app/Exports/BudgetsExport.php
@@ -6,13 +6,19 @@ use App\Budget;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
+use \Maatwebsite\Excel\Sheet;
 
 
-class BudgetsExport implements FromView{
+class BudgetsExport implements FromView, WithEvents, ShouldAutoSize{
 
 	use Exportable;
 
 	protected $budget;
+
+
 
 	public function __construct($budget = null){
 		$this->budget = $budget;
@@ -22,6 +28,29 @@ class BudgetsExport implements FromView{
 		return view('budgets.excel', [
 			'budget' => $this->budget
 		]);
+	}
+
+	public function registerEvents(): array{
+		return [
+			AfterSheet::class =>function(AfterSheet $event) {
+				$styleArray = [
+					'borders' => [
+						'allBorders' => [
+							'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+						]
+					]
+				];
+				$event->sheet->styleCells('A10:E10', $styleArray);
+				$event->sheet->styleCells('A11:E11', $styleArray);
+				$event->sheet->styleCells('A12:E12', $styleArray);
+				$event->sheet->styleCells('A13:L13', $styleArray);
+
+				$event->sheet->styleCells('H10:L10', $styleArray);
+				$event->sheet->styleCells('H11:L11', $styleArray);
+				$event->sheet->styleCells('H12:L12', $styleArray);
+				
+			},
+		];
 	}
 
 	/*public function headings(): array{
