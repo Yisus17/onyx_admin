94541e7f0a430227ad4be90043d52cab203da60a
meche.grc <meche.grc@gmail.com>
2020-06-13 20:26:54 -0400

    fixes in duplicate and update budget

--- onyx_auth/app/Http/Controllers/BudgetController.php
+++ onyx_auth/app/Http/Controllers/BudgetController.php
@@ -32,68 +32,48 @@ class BudgetController extends Controller{
 	}
 
 	public function storeOrUpdate(CreateEditBudgetRequest $request, $id=null){
-		if($id == null){
-			$budget = new Budget($request->all());
-		}else{
+		$editMode = $id != null;
+
+		if($editMode){
 			$budget = Budget::findOrFail($id);
 			$budget->update($request->all());
+		}else{
+			$budget = new Budget($request->all());
 		}
 
 		$client = Client::find($request->client_id);
 		$budget->client()->associate($client);
+		$budget->push();
 
 		$taxBase = 0; //Base imponible
-	
-		if($budget->save()){
-			$products = $request->products;
-			$productsToSync = array();
+		$products = $request->products;
 
-			if(isset($request->products)){
-				foreach ($products as $product){
-					$totalProductPrice = calculateProductTotalPrice($product);
-					$taxBase = $taxBase + $totalProductPrice;
-
-					if($id == null){
-						$budget->products()->attach(
-							$product['id'], 
-							[
-								'description' => $product['description'],
-								'quantity' => $product['quantity'],
-								'factor' => $product['factor'],
-								'unit_price' => $product['unit_price'],
-								'discount' => $product['discount'],
-								'total_price' => $totalProductPrice
-							]
-						);
-					}else{
-						//Update using the incremental id of the many to many relationship
-						$productsToSync[$product['pivot_id']] = array(
-							'product_id'=> $product['id'],
-							'description' => $product['description'],
-							'quantity' => $product['quantity'],
-							'factor' => $product['factor'],
-							'unit_price' => $product['unit_price'],
-							'discount' => $product['discount'],
-							'total_price' => $totalProductPrice
-						);
-					}
-				}
+		if(isset($request->products)){
+			if($editMode) {
+				$budget->products()->detach();
 			}
 			
-			$message = 'Presupuesto creado exitosamente';
+			foreach ($products as $product){
+				$totalProductPrice = calculateProductTotalPrice($product);
+				$taxBase = $taxBase + $totalProductPrice;
 
-			if($id != null){
-				//Sync/update products
-				$budget->products()->sync($productsToSync);
-				$message = 'Presupuesto editado exitosamente';
+				$budget->products()->attach(
+					$product['id'], 
+					[
+						'description' => $product['description'],
+						'quantity' => $product['quantity'],
+						'factor' => $product['factor'],
+						'unit_price' => $product['unit_price'],
+						'discount' => $product['discount'],
+						'total_price' => $totalProductPrice
+					]
+				);
 			}
-			
-			$budget->total = $taxBase; // Add total to budget
-			$budget->save();
-			return redirect('budgets')->with('message', $message);
-		}else{
-			App::abort(500, 'Error');
 		}
+		
+		$message = 'Presupuesto '. ($editMode ? 'editado' : 'creado') .' exitosamente';
+		$budget->total = $taxBase; // Add total to budget
+		return redirect('budgets')->with('message', $message);
 	}
 
 	public function store(CreateEditBudgetRequest $request){
@@ -147,9 +127,22 @@ class BudgetController extends Controller{
 
 	public function duplicate($id){
 		$budgetToDuplicate = Budget::findOrFail($id);
-		$newBudget = $budgetToDuplicate->duplicate();
-		$newBudget->save();
+		$newBudget = $budgetToDuplicate->replicate();
+		$newBudget->push();
 
+		foreach ($budgetToDuplicate->products as $budgetProduct){
+			$newBudget->products()->attach(
+				$budgetProduct->id, 
+				[
+					'description' => $budgetProduct->pivot->description,
+					'quantity' => $budgetProduct->pivot->quantity,
+					'factor' => $budgetProduct->pivot->factor,
+					'unit_price' => $budgetProduct->pivot->unit_price,
+					'discount' => $budgetProduct->pivot->discount,
+					'total_price' => $budgetProduct->pivot->total_price
+				]
+			);
+		}
 		return redirect()->route('budgets.show', [$newBudget->id])->with('message', 'Presupuesto duplicado exitosamente');
 	}
 }
