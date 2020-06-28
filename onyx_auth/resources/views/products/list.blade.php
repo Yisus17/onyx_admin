90943e6c64a679d9a10b39f4d3507aa0b66f9f36
Jesus Arevalo <jaas1710@gmail.com>
2020-06-28 23:34:16 +0200

    Comment register, styling product list and BD inserts

--- onyx_auth/resources/views/products/list.blade.php
+++ onyx_auth/resources/views/products/list.blade.php
@@ -18,7 +18,7 @@
 				</div>
 
 				<div class="card-body">
-					<div class="table-responsive"> 	
+					<div class="table-responsive">
 						<table class="table table-striped">
 							<thead>
 								<tr>
@@ -33,24 +33,25 @@
 							</thead>
 							<tbody>
 								@foreach ($products as $item)
-									<tr>
-										<th scope="row">{{ $item->code }}</th>
-										<td>{{ $item->model }}</td>
-										<td>{{ $item->description }}</td>
-										<td>{{ $item->category->name }}</td>
-										<td>{{ $item->purchase_price ? $item->purchase_price.'€' : ''  }}</td>
-										<td>{{ $item->purchase_date ? $item->purchase_date->format('d/m/Y') : ''}}</td>
-										<td>
-												<form action="{{ route('products.destroy',$item->id) }}" method="POST" onsubmit="return confirm('¿Estás seguro que deseas eliminar a este producto?');">
-														@method('DELETE')
-														@csrf
-														<a href="{{route('products.show', $item->id)}}" class="btn btn-primary btn-sm" title="Edit item"><i class="fas fa-eye"></i></a>
-														<a href="{{route('products.edit', $item)}}" class="btn btn-success btn-sm" title="Edit item"><i class="fas fa-edit"></i></a>
-														<button type="submit" class="btn btn-danger btn-sm" title="Delete item"><i class="fa fa-minus-circle"></i></button>
-												</form>
+								<tr>
+									<th scope="row">{{ $item->code }}</th>
+									<td>{{ $item->model }}</td>
+									<!--Limita caracteres de la celda-->
+									<td>{{\Illuminate\Support\Str::limit($item->description,15 , '...') }}</td>
+									<td>{{ $item->category->name }}</td>
+									<td>{{ $item->purchase_price ? $item->purchase_price.'€' : ''  }}</td>
+									<td>{{ $item->purchase_date ? $item->purchase_date->format('d/m/Y') : ''}}</td>
+									<td id="actions_td">
+										<form action="{{ route('products.destroy',$item->id) }}" method="POST" onsubmit="return confirm('¿Estás seguro que deseas eliminar a este producto?');">
+											@method('DELETE')
+											@csrf
+											<a href="{{route('products.show', $item->id)}}" class="btn btn-primary btn-sm" title="Edit item"><i class="fas fa-eye"></i></a>
+											<a href="{{route('products.edit', $item)}}" class="btn btn-success btn-sm" title="Edit item"><i class="fas fa-edit"></i></a>
+											<button type="submit" class="btn btn-danger btn-sm" title="Delete item"><i class="fa fa-minus-circle"></i></button>
+										</form>
 
-										</td>
-									</tr>
+									</td>
+								</tr>
 								@endforeach
 							</tbody>
 						</table>
--- onyx_auth/routes/web.php
+++ onyx_auth/routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes(['register' => false, 'reset' => false]);
+Auth::routes(['register' => true, 'reset' => false]);
 
 Route::get('/dashboard', 'HomeController@index')->name('dashboard');
 
